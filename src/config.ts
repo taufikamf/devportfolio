89121ff488a877ca3fcc4f99544910f5af3692a9
taufikamf <taufikabdul354@gmail.com>
2025-08-15 18:31:13 +0700

    first commit

---
+++ src/config.ts
@@ -0,0 +1,158 @@
+export const siteConfig = {
+  name: "Taufiq Mufti",
+  title: "Software Developer",
+  description: "Portfolio website of Taufiq Mufti",
+  accentColor: "#1d4ed8",
+  heroImage: "https://i.pinimg.com/736x/27/29/cb/2729cb898bc3ec735fb62b5181eaca76.jpg",
+  social: {
+    email: "taufikabdul354@gmail.com",
+    linkedin: "https://linkedin.com/in/taufikamf",
+    twitter: "https://x.com/inyourlines",
+    github: "https://github.com/taufikamf",
+  },
+  aboutMe:
+    "Results-driven Fullstack Web Developer with 5 years of experience in creating dynamic and scalable web applications. Demonstrated expertise in frontend technologies such as React and Vue, coupled with a strong backend proficiency using Node.js, Express and Go. Experienced in Mobile Development with successful projects on iOS and Android using React Native and Flutter. Passionate about staying ahead in the rapidly evolving tech landscape, solving complex problems, and continuous learning.",
+  skills: ["Javascript", "Typescript", "React", "Vue", "Node.js", "Python", "Go", "Kotlin", "Flutter", "AWS", "Docker"],
+  projects: [
+    {
+      name: "Hadits.ai",
+      description:
+        "A hadith search chatbot system based on Retrieval-Augmented Generation (RAG) that uses Gemini LLM and a vector database (FAISS).",
+      link: "https://github.com/taufikamf/hadits-ai",
+      skills: ["Python", "Vue.js", "LLM"],
+    },
+    {
+      name: "Gudangkita",
+      description:
+        "A comprehensive inventory management system built with Express as Backend and Vue.js as Frontend",
+      link: "https://github.com/taufikamf/gudangkita-frontend",
+      skills: ["Express", "Typescript", "Vue.js"],
+    },
+    {
+      name: "Biomassa",
+      description:
+        "Platform digital untuk integrasi rantai pasok biomassa ke PLTU. Mendukung transaksi, logistik, dan monitoring secara real-time for PLNEPI. building using Kotlin for Mobile and Vue.js with Golang for Website",
+      link: "https://biomassaindonesia.com/",
+      skills: ["Vue.js", "Go", "Kotlin"],
+    },
+    {
+      name: "Glucacore",
+      description:
+        "A diabet and blood sugar Monitoring for elderly build using React Native",
+      link: "https://github.com/taufikamf/glucacore",
+      skills: ["React Native", "Firebase"],
+    },
+  ],
+  experience: [
+    {
+      company: "Digital Aman Sentosa",
+      title: "Information Technology Developer",
+      dateRange: "Jul 2024 - Present",
+      bullets: [
+        "Develop and maintain full-stack applications both web and mobile using Vue, Go and Kotlin.",
+        "Analyze system requirements and translate business needs into technical solutions.",
+        "Collaborate with clients to gather requirements and define system workflows.",
+        "Ensure seamless integration and optimal system performance."
+      ],
+    },
+    {
+      company: "PT Simantep Digital Group",
+      title: "Full Stack Web Developer",
+      dateRange: "Feb 2024 - Jun 2024",
+      bullets: [
+        "Collaborated with cross-functional teams to define, design, and ship new features.",
+        "Built and optimized database structures and queries using PostgreSQL databases.",
+        "Implemented responsive design and ensured cross-browser compatibility.",
+        "Integrated third-party APIs and services to enhance application functionality.",
+        "Participated in Agile/Scrum development processes, contributing to sprint planning, daily stand-ups, and retrospectives."
+      ],
+    },
+    {
+      company: "Seeds Finance",
+      title: "Senior Frontend Developer",
+      dateRange: "Jul 2023 - Jan 2024",
+      bullets: [
+        "Developed and optimized a company profile website using Next.js and TypeScript, implementing SEO strategies that increased organic search traffic by 20%.",
+        "Lead the development of mobile applications for both iOS and Android platforms using React Native, streamlining frontend workflows and code structure for improved efficiency.",
+        "Integrated frontend components with multiple RESTful APIs and collaborated with backend teams to integrate mobile apps with microservices, enhancing data synchronization and ensuring seamless functionality across platforms.",
+      ],
+    },
+    {
+      company: "TaxPrime",
+      title: "Mobile Developer",
+      dateRange: "Apr 2023 - Jul 2023",
+      bullets: [
+        "Developed a high-performance cross-platform mobile application using Flutter for both iOS and Android, ensuring seamless user experience.",
+        "Integrated the app with Firebase for real-time data synchronization and implemented Google OAuth for secure, streamlined user authentication.",
+        "Collaborated with backend developers to integrate the app with microservices, conducting extensive testing and debugging to reduce crash rates by 20% and enhance app stability.",
+      ],
+    },
+    {
+      company: "KunciCoin",
+      title: "Frontend Developer",
+      dateRange: "May 2022 - Mar 2023",
+      bullets: [
+        "Developer and integrated multiple webpages, including kuncicoin.com, kunciplay.com, and community.kuncicoin.com, ensuring a cohesive and engaging user experience.",
+        "Integrated Web3 functionalities, including MetaMask integration, to enable secure blockchain transactions and enhance the platform’s decentralization features.",
+        "Implemented responsive design and optimized site performance, resulting in a 15% incrase in user retention.",
+        "Collaborated with cross-functional teams to allign frontend development with overall project goals and technical requirements.",
+      ],
+    },
+    {
+      company: "Toward Research Business",
+      title: "React Developer",
+      dateRange: "Sep 2021 - Jan 2022",
+      bullets: [
+        "Developed and maintained cryptocurrency-related web platforms, including token websites and exchange platforms, using ReactJS and Redux for scalable state management.",
+        "Built and maintained mobile cryptocurrency exchange applications with React Native, ensuring seamless integration with existing web platforms.",
+        "Performed continuous updates, optimizations, and bug fixes for both web and mobile exchange systems, improving performance and user experience.",
+        "Collaborated with cross-functional teams to implement responsive, real-time features for cryptocurrency trading and portfolio tracking.",
+      ],
+    },
+    {
+      company: "Inti Multi Solusi",
+      title: "Frontend Developer",
+      dateRange: "Sep 2021 - Jan 2022",
+      bullets: [
+        "Developed web and mobile applications using React JS and React Native, ensuring cross-platform compatibility and performance.",
+        "Designed and implemented a Learning Management System (LMS) for an English learning platform, Elingway, enhancing the platform’s educational capabilities and user engagement.",
+        "Collaborated with backend developers to integrate APIs, improving data flow and application functionality.",
+        "Worked closely with UI/UX designers to create intuitive and user-friendly interfaces, increasing user satisfaction and retention.",
+      ],
+    },
+    {
+      company: "Anilo  Adikarya",
+      title: "Backend Developer",
+      dateRange: "Oct 2019 - Mar 2020",
+      bullets: [
+        "Developed RESTful APIs using Laravel to support multiple device applications, ensuring robust and scalable backend services.",
+        "Collaborated with front-end developers to integrate APIs, enhancing overall application functionality and user experience.",
+        "Participated in team-based development projects, delivering websites and applications within specified timeframes.",
+        "Conducted database design and optimization, improving data storage and retrieval efficiency.",
+        "Implemented security best practices to protect application data and ensure compliance with industry standards."
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Universitas Islam Indonesia",
+      degree: "Bachelor of Informatics in Computer Science",
+      dateRange: "2021 - 2025",
+      achievements: [
+        // "Graduated Magna Cum Laude with 3.8 GPA",
+        // "Dean's List all semesters",
+        // "President of Computer Science Club",
+      ],
+    },
+    // {
+    //   school: "Online Platform",
+    //   degree: "Full Stack Development Certificate",
+    //   dateRange: "2019",
+    //   achievements: [
+    //     "Completed 500+ hours of coursework",
+    //     "Built 10+ portfolio projects",
+    //     "Specialized in React and Node.js",
+    //   ],
+    // },
+  ],
+};
